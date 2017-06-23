509d50b9a84c14298cbe71bd8be4357093d75d55
Anuraag Mishra <mishra.anuraag@yahoo.com>
2017-06-23 17:52:17 +0530

    Stripper can help extracting the table but need manual instruction and region naming

--- src/main/java/TableFromPdf/testClasses/TableFromPdfTest.java
+++ src/main/java/TableFromPdf/testClasses/TableFromPdfTest.java
@@ -2,26 +2,47 @@ package TableFromPdf.testClasses;
 
 
 
-
-import com.itextpdf.text.pdf.PdfReader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
 
+import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class TableFromPdfTest {
   public static void run() throws IOException {
-//    File file = new File("C:\\Users\\MAX\\Downloads\\COMMBILLPRINTING23.pdf");
     File file = new File("assests\\PdfWithTable.pdf");
     PDDocument document = PDDocument.load(file);
     PDFTextStripper pdfStripper = new PDFTextStripper();
-    String out = "";
     String text = pdfStripper.getText(document);
+    String lines[] = text.split("\\r?\\n"); // give you all the lines separated by new line
+    System.out.println(Arrays.toString(lines));
+    System.out.println("------------------------------------------------");
+    String cols[] = lines[0].split("\\s+");// gives array separated by whitespaces
+    System.out.println(Arrays.toString(cols));
+    System.out.println("------------------------------------------------");
     System.out.println(text);
+    System.out.println("------------------------------------------------");
+    double w = document.getPage(0).getMediaBox().getWidth();
+    double h = document.getPage(0).getMediaBox().getHeight();
+    System.out.println(w +" , " + h);
+
+    Rectangle2D region = new Rectangle2D.Double(5, 370, w, 50);
+    String regionName = "region";
+    PDFTextStripperByArea stripper;
+
+    stripper = new PDFTextStripperByArea();
+    stripper.addRegion(regionName, region);
+    stripper.extractRegions(document.getPage(0));
+    text = stripper.getTextForRegion(regionName);
+    System.out.println(text);
+
+
     document.close();
 
-    PdfReader reader = new PdfReader("assests\\PdfWithTable.pdf");
+
 
 //    ObjectExtractor oe = new ObjectExtractor(document);
 //    ExtractionAlgorithm extractor = new BasicExtractionAlgorithm();
